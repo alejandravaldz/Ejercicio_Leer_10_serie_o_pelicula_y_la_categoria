0a3cd6cafc3269cb446af2a686b8fc061184b761
Alejandra Valdez <valdezmoralesalejandramaciel@gmail.com>
2024-02-18 23:01:24 -0500

    Agregar archivos de proyecto.

---
+++ series y pelicula usando arreglo/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Mostrar la serie o pelicula y la categoria");
+Console.WriteLine();
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine("Ingrese el nombre de la serie de películas " + i + ":");
+    string nombre = Console.ReadLine();
+
+    Console.WriteLine("Ingrese la categoría de la serie de películas " + i + ":");
+    string categoria = Console.ReadLine();
+    Console.WriteLine();
+
+    Console.WriteLine("Datos de la serie de películas " + i + ":");
+    Console.WriteLine("Nombre: " + nombre);
+    Console.WriteLine("Categoría: " + categoria);
+    Console.WriteLine();
+}
+   
+
+
+
+
+
+
